52f52b78f1d91a002603d04344c9560c7ca67388
Volodymyr Honchar <vhonchar94@gmail.com>
2025-12-17 22:43:22 -0800

    Add and mount necessary EBS volumes

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,9 +1,10 @@
 locals {
-  # user_data.sh.tpl takes no variables now
-  user_data = templatefile("user_data.sh.tftpl", {
-    mounts_bash = join("\n", [
-      for k, v in var.matrix_volumes :
-      "MOUNTS[\"${v.device_name}\"]=\"${v.mount_path}\""
-    ])
+  mounts_bash = join("\n", [
+    for _, v in var.matrix_volumes :
+    "MOUNTS[\"${v.device_name}\"]=\"${v.mount_path}\""
+  ])
+
+  user_data = templatefile("${path.module}/user_data.sh.tftpl", {
+    mounts_bash = local.mounts_bash
   })
 }
