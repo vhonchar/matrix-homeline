22c8ee627e2ae4a9ffe47dab04511d385a35e613
Volodymyr Honchar <vhonchar94@gmail.com>
2025-12-19 22:28:07 -0800

    Add more permissions to EC2 instance

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -41,7 +41,8 @@ resource "aws_iam_role_policy" "ec2_read_matrix_homeline_params" {
           "ssm:GetParametersByPath"
         ]
         Resource = [
-          "arn:aws:ssm:eu-central-1:${data.aws_caller_identity.current.account_id}:parameter/matrix-homeline/prod/*"
+          "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/matrix-homeline/prod",
+          "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/matrix-homeline/prod/*",
         ]
       }
     ]
